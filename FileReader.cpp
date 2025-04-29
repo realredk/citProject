b0f1079904136da52667850d67bf2ce205ef2dee
Ziyilii <149326585+Ziyilii@users.noreply.github.com>
2025-04-29 16:52:41 -0400

    Update FileReader.cpp

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,13 +1,17 @@
 #include "FileReader.hpp"
 #include <fstream>
 #include <sstream>
-#include <optional>
+
+namespace searchserver {
 
 std::optional<std::string> read_file(const std::string& path) {
     std::ifstream ifs(path, std::ios::binary);
-    if (!ifs) return std::nullopt;
+    if (!ifs) {
+        return std::nullopt;
+    }
     std::ostringstream oss;
     oss << ifs.rdbuf();
     return oss.str();
+}
 
 }  // namespace searchserver
